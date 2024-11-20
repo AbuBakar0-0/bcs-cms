b87afccd5c4b2c2c4c1ea6eb4932a8ac8a4d44a3
Shahzaib Ali <abubakar.80187@gmail.com>
2024-11-20 10:38:32 +0500

    hdsajdhjkdsah

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,15 +2,17 @@ import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
 export const metadata = {
-	title: "BCS CMS",
-	description: "Credentialing Management System",
+  title: "BCS CMS",
+  description: "Credentialing Management System",
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<body className="font-custom">{children}</body>
-			<Toaster position="top-center" />
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className="font-custom">
+        <Toaster position="top-center" />
+        {children}
+      </body>
+    </html>
+  );
 }
