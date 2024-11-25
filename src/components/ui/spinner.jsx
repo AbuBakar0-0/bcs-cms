d78939888166db150a29e9f213386597d54fb32b
Abu Bakar <abubakar.80187@gmail.com>
2024-11-25 14:10:06 +0500

    jk]

--- src/components/ui/spinner.jsx
+++ src/components/ui/spinner.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Spinner() {
+function Spinner() { 
   return (
     <div className='w-full flex justify-center items-center gap-4'>
       <div className='rounded-full size-10 border-t-4 border-x-4 animate-spin border-secondary'></div>
